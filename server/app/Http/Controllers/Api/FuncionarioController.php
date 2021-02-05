958265e6c16da77c70f68f6d6ea330db684a4db6
Jose Victor Pacajoj Mejia <jovipac@hotmail.com>
2021-02-04 23:45:47 -0600

    Fix a consulta SQL de listado de detalles del funcionario

--- server/app/Http/Controllers/Api/FuncionarioController.php
+++ server/app/Http/Controllers/Api/FuncionarioController.php
@@ -27,12 +27,12 @@ class FuncionarioController extends ApiController
             'T04.id_auxiliatura', 'T04.nombre AS nombre_auxiliatura',
             'T05.id_rol', 'T06.nombre AS nombre_rol',
             'tc_funcionario.created_at', 'tc_funcionario.updated_at')
-        ->leftJoin('ts_usuario as T01', 'tc_funcionario.id_funcionario', '=', 'T01.id_funcionario')
+        ->join('ts_usuario as T01', 'tc_funcionario.id_funcionario', '=', 'T01.id_funcionario')
         ->leftJoin('tc_puesto as T02', 'T02.id_puesto', '=', 'tc_funcionario.id_puesto')
         ->leftJoin('tc_dependencia as T03', 'T02.id_dependencia', '=', 'T03.id_dependencia')
         ->leftJoin('tc_auxiliatura as T04', 'T04.id_auxiliatura', '=', 'T01.id_auxiliatura')
         ->leftJoin('tt_usuario_rol as T05', 'T05.id_usuario', '=', 'T01.id_usuario')
-        ->leftJoin('ts_rol as T06', 'T06.id_rol', '=', 'T05.id_rol')
+        ->join('ts_rol as T06', 'T06.id_rol', '=', 'T05.id_rol')
         ->get();
 
         return $this->apiResponse(
@@ -78,7 +78,7 @@ class FuncionarioController extends ApiController
             'email' => 'email',
             'id_puesto' => 'required|integer',
             'id_auxiliatura' => 'required|integer',
-            'id_rol' => 'integer',
+            'id_rol' => 'required|integer',
         ]);
         if ($validator->fails()) {
             return $this->respondError($validator->errors(), 422);
@@ -150,7 +150,7 @@ class FuncionarioController extends ApiController
             'apellidos' => 'required',
             'email' => 'email',
             'id_puesto' => 'required|integer',
-            'id_rol' => 'integer',
+            'id_rol' => 'required|integer',
         ]);
         if ($validator->fails()) {
             return $this->respondError($validator->errors(), 422);
