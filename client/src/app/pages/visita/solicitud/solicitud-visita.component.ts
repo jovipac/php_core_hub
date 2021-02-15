37dd778505da6ee65e8ea1f199cd42ef4c2bef22
Jose Victor Pacajoj Mejia <jovipac@hotmail.com>
2021-02-15 10:07:45 -0600

    Se sube fix temporal para que pueda compilarse el proyecto angular

--- client/src/app/pages/visita/solicitud/solicitud-visita.component.ts
+++ client/src/app/pages/visita/solicitud/solicitud-visita.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 @Component({
   selector: 'app-solicitud-visita',
   templateUrl: './solicitud-visita.component.html',
   styleUrls: ['./solicitud-visita.component.scss']
 })
 export class SolicitudVisitaComponent implements OnInit {
+  solicitud: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.buildForm();
+   }
 
   ngOnInit() {
+  }
+
+  private buildForm() {
+    this.solicitud = this.formBuilder.group({
+      cui: ["", [Validators.required]],
+      nombres: ["", [Validators.required]],
+      apellidos: ["", [Validators.required]],
+    });
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+
   }
 
 }
--- client/src/app/pages/visita/solicitud/solicitud-visita.module.ts
+++ client/src/app/pages/visita/solicitud/solicitud-visita.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { SolicitudVisitaRoutingModule } from './solicitud-visita-routing.module';
 import { SolicitudVisitaComponent } from './solicitud-visita.component';
-import {SharedModule} from '../../../shared/shared.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     SolicitudVisitaRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [SolicitudVisitaComponent]
 })
