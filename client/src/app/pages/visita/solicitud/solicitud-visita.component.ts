5b2838f39c3617992b0c9b179ae05ed1cf3e1b73
Jose Victor Pacajoj Mejia <jovipac@hotmail.com>
2021-02-18 02:49:11 -0600

    Se agrega la logistica de la validacion del formulario registrar visita

--- client/src/app/pages/visita/solicitud/solicitud-visita.component.ts
+++ client/src/app/pages/visita/solicitud/solicitud-visita.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
 import { ServicesService } from "../../../service/services.service";
 import { first } from 'rxjs/operators';
 
@@ -20,15 +20,17 @@ interface auxiliary {
 
 export class SolicitudVisitaComponent implements OnInit {
   public visitaForm: FormGroup;
-  public isAddMode: boolean;
   id: string;
+  isAddMode: boolean;
+  loading = false;
+  submitted = false;
   public listDependency: Array<dependency>;
   public listAuxiliary: Array<auxiliary>;
 
   constructor(
     private route: ActivatedRoute,
     private commonService: ServicesService,
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
   ) {
     this.buildForm();
   }
@@ -46,21 +48,33 @@ export class SolicitudVisitaComponent implements OnInit {
     }
   }
 
+  dateLessThan(from: string) {
+    return (group: FormGroup): {[key: string]: any} => {
+      let f = group.controls[from];
+      if (f.value < new Date()) {
+        return {
+          dates: "Date from should be less than Date"
+        };
+      }
+      return {};
+    }
+  }
+
   private buildForm() {
-    this.visitaForm = this.formBuilder.group({
-      id_persona: ["", []],
-      cui: ["", [Validators.required]],
-      nombres: ["", [Validators.required]],
-      apellidos: ["", [Validators.required]],
-      telefono: ["", []],
-      fecha_nacimiento: ["", []],
-      edad: ["", []],
-      id_sexo: ["", []],
-      id_motivo: ["", []],
-      id_dependencia: ["", [Validators.required]],
-      id_funcionario: ["", [Validators.required]],
-      llamadas: [0, []],
-    });
+    this.visitaForm = this.fb.group({
+      id_persona: ['', [Validators.pattern("[0-9]")]],
+      cui: ['', [Validators.required, Validators.pattern("[0-9]{12,14}")]],
+      nombres: ['', [Validators.required]],
+      apellidos: ['', [Validators.required]],
+      telefono: ['', [Validators.pattern("[0-9]{8,10}")]],
+      fecha_nacimiento: ['', []],
+      edad: ['', [Validators.pattern("[0-9]")]],
+      id_sexo: ['', [Validators.pattern("[0-9]")]],
+      id_motivo: ['', [Validators.pattern("[0-9]")]],
+      id_dependencia: ['', [Validators.required, Validators.pattern("[0-9]")]],
+      id_funcionario: ['', [Validators.required, Validators.pattern("[0-9]")]],
+      llamadas: [0, [Validators.pattern("[0-9]")]],
+    }, { validators: this.dateLessThan('fecha_nacimiento') });
   }
 
   getListDependecy() {
@@ -87,7 +101,27 @@ export class SolicitudVisitaComponent implements OnInit {
     })
   }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.visitaForm.controls; }
+
   onSubmit() {
+    this.submitted = true;
+    console.log("Probando")
+    // stop here if form is invalid
+    if (this.visitaForm.invalid) {
+      console.log(this.visitaForm)
+      return;
+    } else {
+      console.log(this.visitaForm.value)
+    }
+
+    this.loading = true;
+    if (this.isAddMode) {
+        //this.createVisita();
+    } else {
+        //this.updateVisita();
+    }
+
   }
 
 }
