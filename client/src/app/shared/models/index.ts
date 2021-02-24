c74cf1376f14fdcdbacdb22412bfbb71db28d601
Jose Victor Pacajoj Mejia <jovipac@hotmail.com>
2021-02-24 12:02:44 -0600

    Quick fix para agregar la interfaz de Genero

--- client/src/app/shared/models/index.ts
+++ client/src/app/shared/models/index.ts
@@ -1,5 +1,6 @@
 export * from './prioridad.interface';
 export * from './sexo.interface';
+export * from './genero.interface';
 export * from './motivo.interface';
 export * from './visita.interface';
 export * from './funcionario.interface';
