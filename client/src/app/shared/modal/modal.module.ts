101c5c712bb775c2fb1ed77c7208a5a9d297513f
Jose Victor Pacajoj Mejia <jovipac@hotmail.com>
2021-03-28 23:15:05 -0600

    Fix en mostrar listado archivos adjunto del hecho del expediente

--- client/src/app/shared/modal/modal.module.ts
+++ client/src/app/shared/modal/modal.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { UiModalComponent } from './ui-modal/ui-modal.component';
 import { AnimationModalComponent } from './animation-modal/animation-modal.component';
 import { BasicModalComponent } from './basic-modal/basic-modal.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ModalModule,
   ],
   declarations: [BasicModalComponent, UiModalComponent, AnimationModalComponent],
   exports: [BasicModalComponent, UiModalComponent, AnimationModalComponent],
