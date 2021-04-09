b09d798da93a5c3934c92bcc60c4ba48dcd659b4
emazariegosc@miumg.edu.gt <emalug>
2021-04-08 22:15:09 -0600

    Servicios de condiocion de escolaridad

---
+++ server/database/migrations/2021_04_09_041030_create_escolaridads_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEscolaridadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tc_escolaridad', function (Blueprint $table) {
+            $table->increments('id_escolaridad');
+            $table->string('nombre', 80);
+            $table->string('descripcion')->nullable();
+            $table->boolean('borrado')->default(0)->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tc_escolaridad');
+    }
+}
