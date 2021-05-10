9f0be17459b1bfb6afec5b4af214e76b299e5771
Jose Victor Pacajoj Mejia <jovipac@hotmail.com>
2021-05-09 22:59:49 -0600

    Quick fix en parametros del formulario de contraseña olvidada

--- client/src/app/pages/account/reset-password.component.ts
+++ client/src/app/pages/account/reset-password.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
 import { AuthService, AlertService } from '../../service';
-import { MustMatch } from '../../shared/utils';
+import { extractErrorMessages, MustMatch } from '../../shared/utils';
 
 enum TokenStatus {
     Validating,
@@ -73,8 +73,9 @@ export class ResetPasswordComponent implements OnInit {
 
         this.loading = true;
         const formValues = {
-          ...this.form.value,
           email: this.email,
+          password: this.f.password.value,
+          password_confirmation: this.f.confirmPassword.value,
           token: this.token,
         }
         this.authService.resetPassword(formValues)
@@ -84,8 +85,17 @@ export class ResetPasswordComponent implements OnInit {
                     this.alertService.success('Password reset successful, you can now login', { keepAfterRouteChange: true });
                     this.router.navigate(['../login'], { relativeTo: this.route });
                 },
-                error: error => {
-                    this.alertService.error(error);
+                error: response => {
+                    if (Object.prototype.toString.call(response.error.message) === '[object Object]') {
+                      const messages = extractErrorMessages(response);
+                      messages.forEach(propertyErrors => {
+                        for (let message in propertyErrors) {
+                          this.alertService.error(propertyErrors[message]);
+                        }
+                      });
+                    } else {
+                      this.alertService.error(response.error.message)
+                    }
                     this.loading = false;
                 }
             });
